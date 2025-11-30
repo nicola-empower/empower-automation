f9560377d3f1b021bc49745678648556b25d72e7
Nicola Berry <nicola@empowervaservices.co.uk>
2025-11-30 01:40:56 +0000

    vercel issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,14 +6,11 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 
 // https://astro.build/config
-import vercel from '@astrojs/vercel';
-
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
 
   integrations: [react()],
-  output: 'static',
-  adapter: vercel()
+  output: 'static'
 });
